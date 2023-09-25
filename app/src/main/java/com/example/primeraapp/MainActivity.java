3b1efcfd895d4376ba1d07b32a976b88a45f1aee
jmartintoro <jmartintoro.cf@iesesteveterradas.cat>
2023-09-25 20:14:35 +0200

    button + Toast

--- app/src/main/java/com/example/primeraapp/MainActivity.java
+++ app/src/main/java/com/example/primeraapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.primeraapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CharSequence text = "Primer Toast!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(getApplicationContext(),text,duration);
+                toast.show();
+            }
+        });
     }
 }
